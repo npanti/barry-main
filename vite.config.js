5628e60994050f191768d2ab1c47f1ae1153fa40
Nicolas <npanti@gmail.com>
2025-07-28 09:24:00 +0200

    update base

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,10 @@ import { defineConfig } from "vite";
 import pugPlugin from "vite-plugin-pug";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command }) => ({
+export default defineConfig(() => ({
   plugins: [pugPlugin()],
-  base: command === 'build' ? '/barry-main/' : '/',
+  // base: command === 'build' ? '/barry-main/' : '/',
+  base: '/barry-main/',
   build: {
     outDir: "docs",
   },
